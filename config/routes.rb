21bc13571ef8994bff077cb526ec6d3b0d473b77
Gautam Moradiya <gautam.moradiya@bacancytechnology.com>
2020-02-03 20:34:03 +0530

    added root url

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  root 'git_org_analyzers#open_source_repositories'
   get '/git_analysis' => 'git_org_analyzers#open_source_repositories'
 end
